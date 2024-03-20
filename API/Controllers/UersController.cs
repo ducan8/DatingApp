a7568d8cb75f13807188e3c7d477723b441ef6cc
ducan8 <ducan9c@gmail.com>
2024-03-20 17:32:47 +0700

    initial first commit

---
+++ API/Controllers/UersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UersController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public UersController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserApp>>> GetAllUser()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<UserApp>> GetUser(int id)
+        {
+            return await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public virtual DbSet<UserApp> Users { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
